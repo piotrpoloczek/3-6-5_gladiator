36d3dba51ca781500f9c27cf98295fc1090a517b
piotrpoloczek <piotr.jan.poloczek@gmail.com>
2023-05-02 14:20:06 +0200

    add subclass for Gladiator

--- src/main/java/com/codecool/gladiator/view/ConsoleView.java
+++ src/main/java/com/codecool/gladiator/view/ConsoleView.java
@@ -1,19 +1,37 @@
 package com.codecool.gladiator.view;
 
+import java.util.Scanner;
+
 /**
  * Basic console view implementation
  */
 public class ConsoleView implements Viewable {
 
+    private final Scanner scanner;
+
+    public ConsoleView() {
+        this.scanner = new Scanner(System.in);
+    }
+
     @Override
     public void display(String text) {
-        // Todo
+        System.out.println(text);
     }
 
     @Override
     public int getNumberBetween(int min, int max) {
-        // Todo
-        return 2;
-    }
+        while (true) {
+            System.out.printf(
+                    "Please provide the number between %s and %s",
+                    min, max);
 
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice >= min && choice <= max) {
+                return choice;
+            }
+            System.out.println("Invalid choice. Please try again.");
+        }
+    }
 }
